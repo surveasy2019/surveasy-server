03bf0d802d8f0d4aff25474532270281d3fdefe4
seoyamin <seoyamin@ewhain.net>
2023-08-08 11:42:50 +0900

    [init] Init the Project

---
+++ src/main/java/surveasy/SurveasyApplication.java
@@ -0,0 +1,13 @@
+package surveasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/surveasy/SurveasyApplicationTests.java
@@ -0,0 +1,13 @@
+package surveasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
