32f4037a1a4e2658c59e7b078b732ad0c9c859d8
seoyamin <seoyamin@ewhain.net>
2024-03-19 08:34:49 +0900

    [refactor] Server TimeZone Asia/Seoul 설정

--- src/main/java/surveasy/SurveasyApplication.java
+++ src/main/java/surveasy/SurveasyApplication.java
@@ -1,8 +1,11 @@
 package surveasy;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,8 @@ public class SurveasyApplication {
 		SpringApplication.run(SurveasyApplication.class, args);
 	}
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
