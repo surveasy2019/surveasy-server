2ca2ef8d199e6bbf6161af253032deb8ac86a591
seoyamin <seoyamin@ewhain.net>
2024-04-13 17:30:47 +0900

    [refacotr] ResponseHistoryVo resposeStatus, surveyStatus 추가 #88

--- src/main/java/surveasy/domain/response/vo/ResponseHistoryVo.java
+++ src/main/java/surveasy/domain/response/vo/ResponseHistoryVo.java
@@ -2,6 +2,8 @@ package surveasy.domain.response.vo;
 
 import lombok.Builder;
 import lombok.Getter;
+import surveasy.domain.response.domain.ResponseStatus;
+import surveasy.domain.survey.domain.SurveyStatus;
 import surveasy.global.common.util.DateAndStringUtils;
 
 import java.time.LocalDate;
@@ -22,14 +24,19 @@ public class ResponseHistoryVo {
 
     private String sentAt;
 
+    private ResponseStatus responseStatus;
+    private SurveyStatus surveyStatus;
+
     @Builder
     public ResponseHistoryVo(Long id, String surveyTitle, Integer surveyReward, String imgUrl,
-                             LocalDateTime createdAt, LocalDate sentAt) {
+                             LocalDateTime createdAt, LocalDate sentAt, ResponseStatus responseStatus, SurveyStatus surveyStatus) {
         this.id = id;
         this.surveyTitle = surveyTitle;
         this.surveyReward = surveyReward;
         this.imgUrl = imgUrl;
         this.createdAt = DateAndStringUtils.localDateTimeToString(createdAt);
         if(sentAt != null) this.sentAt = DateAndStringUtils.localDateToString(sentAt);
+        this.responseStatus = responseStatus;
+        this.surveyStatus = surveyStatus;
     }
 }
